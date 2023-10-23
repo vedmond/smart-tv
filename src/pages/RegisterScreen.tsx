0ed546e99477d92489edf9a7c9b8f3ff1c5a8482
vedmond <vyedkh@gmail.com>
2023-10-23 19:49:26 +0300

    add UI for Register page

--- src/pages/RegisterScreen.tsx
+++ src/pages/RegisterScreen.tsx
@@ -3,6 +3,22 @@ import { AiOutlineClose } from 'react-icons/ai'
 import { IProps } from '../types/interface'
 
 export const RegisterScreen = ({ setScreenName, setPlayerTime }: IProps) => {
+  const phoneNumberSample = '+7(___)_ _ _-_ _-_ _'
+  const keyboardItems = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'стереть',
+    '0',
+  ]
+  console.log(phoneNumberSample.split('').join(''))
+
   useEffect(() => {
     const time = localStorage.getItem('videoTime')
     if (time) {
@@ -14,7 +30,37 @@ export const RegisterScreen = ({ setScreenName, setPlayerTime }: IProps) => {
   }
   return (
     <>
-      <div className="register"></div>
+      <div className="register">
+        <div className="titleRegister"></div>
+        <div className="phoneNumberBlock">{phoneNumberSample}</div>
+        <div className="textRegister"></div>
+        <div className="virtualKeyboardBlock">
+          {keyboardItems.map((el) => (
+            <div
+              key={el}
+              className={`${
+                el === 'стереть'
+                  ? 'virtualKeyboardItem__Backspace'
+                  : 'virtualKeyboardItems'
+              }`}
+            >
+              <span>{el}</span>
+            </div>
+          ))}
+        </div>
+        <div className="checkPersonalDataBlock">
+          <div className="checkbox">
+            <input
+              className="checkbox__input"
+              type="checkbox"
+              id="checkbox_1"
+            />
+            <label className="checkbox__label" htmlFor="checkbox_1"></label>
+          </div>
+          <div className="textCheckbox"></div>
+        </div>
+        <button className="btnVirtualKeyboard">ПОДТВЕРДИТЬ НОМЕР</button>
+      </div>
       <button className="btnExit" onClick={onClick}>
         <AiOutlineClose />
       </button>
