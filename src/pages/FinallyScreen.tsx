f916e358ac73ec0c1b081b351b0c8bc8bff4df0a
vedmond <vyedkh@gmail.com>
2023-10-28 11:12:41 +0300

    add a transition from page Finally to Promo

--- src/pages/FinallyScreen.tsx
+++ src/pages/FinallyScreen.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { IProps } from '../types/interface'
 import { ExitButton } from '../components/ExitButton'
+import { keyEventFilter } from '../utils/keyEventFilter'
 
 export const FinallyScreen = ({ setScreenName, setPlayerTime }: IProps) => {
+  const handleOnFocus = 'exit-itm'
+  useEffect(() => {
+    const onKeyUpEvent = (event: any) => {
+      if (event.key === 'Enter') {
+        if (setScreenName) setScreenName('promo')
+      }
+    }
+    document.addEventListener('keyup', onKeyUpEvent)
+    return () => {
+      document.removeEventListener('keyup', onKeyUpEvent)
+    }
+  }, [])
+  if (setPlayerTime) setPlayerTime(0)
+
+  const onClickExit = () => {
+    console.log('onClickExit')
+    if (setScreenName) setScreenName('promo')
+  }
+
   return (
     <>
       <div className="finally">
         <div className="finally__text"></div>
       </div>
-      <ExitButton />
+      <ExitButton onClickExit={onClickExit} handleOnFocus={handleOnFocus} />
     </>
   )
 }
--- src/utils/keyEventFilter.ts
+++ src/utils/keyEventFilter.ts
@@ -2,9 +2,9 @@ import React, { Dispatch, useEffect } from 'react'
 import { arrowItems, keyboardItems } from '../constants'
 
 export const keyEventFilter = (
-  setPressKeyNumber: Dispatch<React.SetStateAction<string>>,
-  setPressKeyArrow: Dispatch<React.SetStateAction<string>>,
-  setPressKeyEnter: Dispatch<React.SetStateAction<string>>,
+  setPressKeyNumber?: Dispatch<React.SetStateAction<string>>,
+  setPressKeyArrow?: Dispatch<React.SetStateAction<string>>,
+  setPressKeyEnter?: Dispatch<React.SetStateAction<string>>,
 ) => {
   return (event: any) => {
     event.preventDefault()
@@ -12,8 +12,14 @@ export const keyEventFilter = (
     const filterEnter = 'Enter'
     const filterArrow = [...arrowItems]
     const filterNumber = [...keyboardItems, 'Backspace']
-    if (filterEnter.includes(event.key)) setPressKeyEnter(event.key)
-    if (filterArrow.includes(event.key)) setPressKeyArrow(event.key)
-    if (filterNumber.includes(event.key)) setPressKeyNumber(event.key)
+    if (filterEnter.includes(event.key)) {
+      if (setPressKeyEnter) setPressKeyEnter(event.key)
+    }
+    if (filterArrow.includes(event.key)) {
+      if (setPressKeyArrow) setPressKeyArrow(event.key)
+    }
+    if (filterNumber.includes(event.key)) {
+      if (setPressKeyNumber) setPressKeyNumber(event.key)
+    }
   }
 }
